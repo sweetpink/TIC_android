ce9dfc210dcfe95bf16a39f58efb5c0199ef115c
HHenrik <henrikasman@yahoo.se>
2016-03-24 17:47:54 +0100

    test

--- app/src/main/java/com/example/henrik/facebook_test_rightversion/MainMenu.java
+++ app/src/main/java/com/example/henrik/facebook_test_rightversion/MainMenu.java
@@ -16,7 +16,7 @@ public class MainMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
         alarmChecker();
-        System.out.print("dddd");
+        System.out.print("Ändrat henrik");
     }
 
 
